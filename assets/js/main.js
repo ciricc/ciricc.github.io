4b3aeb4c0e107c9b3dc0d7485319e7716ff2705b
ciricc <facevk.02@mail.ru>
2018-11-09 01:30:05 +0300

    Updated highlighter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
 !(function(){
 	"use strict";
 
+
+	Prism.plugins.NormalizeWhitespace.setDefaults({
+		'remove-trailing': true,
+		'remove-indent': true,
+		'left-trim': true,
+		'right-trim': true,
+		// 'break-lines': 80,
+		// 'indent': 2,
+		'remove-initial-line-feed': false,
+		// 'tabs-to-spaces': 4,
+		'spaces-to-tabs': 4
+	});
+
 	var $ = jQuery;
 
 	jQuery.fn.justtext = function() {
