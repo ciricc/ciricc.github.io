50165c1975b3dc6ba24fc3420402fabc53598399
ciricc <facevk.02@mail.ru>
2020-05-12 09:23:46 +0300

    ChatBot Tools alpha added

---
+++ cbtools/app/alpha/precache-manifest.63bc7ef2578885e3d96ed0cc2b3f91c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b33d37dec8d25f62d37fe9a9760374c",
+    "url": "/cbtools/app/alpha/index.html"
+  },
+  {
+    "revision": "45a2cc0f7d707e30cead",
+    "url": "/cbtools/app/alpha/static/css/2.e5597a30.chunk.css"
+  },
+  {
+    "revision": "f6bf7b3daeff3c85bf8a",
+    "url": "/cbtools/app/alpha/static/css/main.5f5b65c1.chunk.css"
+  },
+  {
+    "revision": "45a2cc0f7d707e30cead",
+    "url": "/cbtools/app/alpha/static/js/2.fb486556.chunk.js"
+  },
+  {
+    "revision": "f39f4825984579905b977634b7b98e06",
+    "url": "/cbtools/app/alpha/static/js/2.fb486556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6bf7b3daeff3c85bf8a",
+    "url": "/cbtools/app/alpha/static/js/main.1d90dccc.chunk.js"
+  },
+  {
+    "revision": "cd6389993555b75494dd",
+    "url": "/cbtools/app/alpha/static/js/runtime-main.23072f5e.js"
+  }
+]);
